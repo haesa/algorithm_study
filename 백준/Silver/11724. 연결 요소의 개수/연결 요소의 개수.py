6a346e991acb3a90e89ff24100c2e744b79e36d9
haesa <34948133+haesa@users.noreply.github.com>
2026-03-27 21:15:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 612 ms, Memory: 67060 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+
+def bfs(v):
+    q = deque([v])
+    visited[v] = 1
+
+    while q:
+        v = q.popleft()
+
+        for u in graph[v]:
+            if not visited[u]:
+                q.append(u)
+                visited[u] = 1
+
+
+count = 0
+for i in range(1, N + 1):
+    if not visited[i]:
+        bfs(i)
+        count += 1
+
+print(count)
