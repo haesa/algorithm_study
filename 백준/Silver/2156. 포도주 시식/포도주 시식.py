f8330e30fb2a3f3088475d26c256b20d29c82747
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:27:05 +0900

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,22 +1,32 @@
 import sys
-read = sys.stdin.readline
+input = sys.stdin.readline
 
-N = int(read())
-A = []
-for _ in range(N):
-    A.append(int(read()))
+'''
+DP[i] = i번째 잔까지 포도주의 최대양
+DP[i] = DP[i - 1] # 안 마시는 경우
+DP[i] = wines[i] + wines[i - 1] + DP[i - 3] # 이어서 마시는 경우
+DP[i] = wines[i] + DP[i - 2]    # 1잔 건너뛰는 경우
+DP[i] = wines[i] + DP[i - 3]    # 2잔 건너뛰는 경우
+둘 중 더 큰 경우로 업데이트
+'''
 
-DP = [0] * N
+N = int(input())
+wines = [int(input()) for _ in range(N)]
 
 if N == 1:
-    print(A[0])
+    print(wines[0])
 elif N == 2:
-    print(A[0] + A[1])
+    print(wines[0] + wines[1])
+elif N == 3:
+    print(max(wines[0] + wines[1], wines[1] + wines[2], wines[0] + wines[2]))
 else:
-    DP[0] = A[0]
-    DP[1] = A[0] + A[1]
-    DP[2] = max(A[0] + A[1], A[1] + A[2], A[0] + A[2])
-    for i in range(3, N):
-        DP[i] = max(DP[i - 1], A[i] + DP[i - 2], A[i] + A[i - 1] + DP[i - 3])
+    DP = [0] * N
+    DP[0] = wines[0]
+    DP[1] = wines[0] + wines[1]
+    DP[2] = max(wines[0] + wines[1], wines[1] + wines[2], wines[0] + wines[2])
 
-    print(DP[N - 1])
+    for i in range(3, N):
+        DP[i] = max(DP[i - 1], wines[i] +
+                    wines[i - 1] + DP[i - 3], wines[i] + DP[i - 2], wines[i] + DP[i - 3])
+
+    print(max(DP))
