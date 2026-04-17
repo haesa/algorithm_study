e4f74a5bd288d83838b6a298f22bab104d324e76
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:29:02 +0900

    [Gold V] Title: 합분해, Time: 44 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -3,26 +3,11 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-MOD = 1_000_000_000
 
-"""
-DP[i][j] = 0 ~ i까지 j개의 수를 골라 i를 만드는 경우의 수
-
-DP[N][K] = DP[N][K - 1] + DP[N - 1][K - 1] + DP[N - 2][K - 1] + ... + DP[0][K - 1]
-DP[N - 1][K] = DP[N - 1][K - 1] + DP[N - 2][K - 1] + ... + DP[0][K - 1]
-DP[N][K] = DP[N][K - 1] + DP[N - 1][K]
-"""
-
-DP = [[0] * (K + 1) for _ in range(N + 1)]
-
-for i in range(N + 1):
-    DP[i][1] = 1
-
-for i in range(1, K + 1):
-    DP[0][i] = 1
+DP = [[1] * (N + 1) for _ in range(K + 1)]
 
 for i in range(2, K + 1):
-    for j in range(1, N + 1):
-        DP[j][i] = (DP[j][i - 1] + DP[j - 1][i]) % MOD
+  for j in range(1, N + 1):
+    DP[i][j] = (DP[i - 1][j] + DP[i][j - 1]) % 1_000_000_000
 
-print(DP[N][K])
+print(DP[K][N])
