e080bdba284d6598f92b2cca5ea09559f2b2bae5
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:34:25 +0900

    [Gold V] Title: 탑, Time: 588 ms, Memory: 173612 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.js
+++ 백준/Gold/2493. 탑/탑.js
@@ -6,9 +6,9 @@ const stack = [];
 const result = [];
 
 for (let i = 0; i < n; i++) {
-  while (stack.length && tower[stack.at(-1)] < tower[i]) stack.pop();
+  while (stack.length && tower[stack[stack.length - 1]] < tower[i]) stack.pop();
   if (stack.length === 0) result.push(0);
-  else result.push(stack.at(-1) + 1);
+  else result.push(stack[stack.length - 1] + 1);
   stack.push(i);
 }
 console.log(...result);
