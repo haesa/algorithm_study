93053cfa55daa062cc9058a96e93b24f2682dcdc
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:12:02 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 124 ms, Memory: 11212 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const [n, k] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+const result = [];
+const arr = [];
+for (let i = 1; i <= n; i++) arr.push(i);
+
+let pos = 0;
+while (arr.length) {
+  pos = (pos + k - 1) % arr.length;
+  result.push(arr.splice(pos, 1));
+}
+console.log('<' + result.join(', ') + '>');
