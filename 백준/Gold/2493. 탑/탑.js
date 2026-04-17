86f18fad22098d544d69e8a4727f6f73b14f56bf
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:36:37 +0900

    [Gold V] Title: 탑, Time: 432 ms, Memory: 122720 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.js
+++ 백준/Gold/2493. 탑/탑.js
@@ -1,16 +1,14 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const n = Number(input.shift());
-const tower = input[0].split(' ').map(Number);
+const n = Number(input[0]);
+const tower = input[1].split(' ').map(Number);
 const stack = [];
 const result = [];
 
 for (let i = 0; i < n; i++) {
-  const curTower = tower[i];
-  while (stack.length && tower[stack.at(-1)] < curTower) stack.pop();
-  if (!stack.length) result.push(0);
-  else result.push(stack.at(-1) + 1);
+  while (stack.length && tower[stack[stack.length - 1]] < tower[i]) stack.pop();
+  if (stack.length === 0) result.push(0);
+  else result.push(stack[stack.length - 1] + 1);
   stack.push(i);
 }
-console.log(...result);
+console.log(result.join(' '));
