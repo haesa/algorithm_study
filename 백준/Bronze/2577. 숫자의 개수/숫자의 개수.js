e419406a31c44e162c6f0aa6fae56eea2965ae43
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:40:14 +0900

    [Bronze II] Title: 숫자의 개수, Time: 124 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
@@ -1,14 +1,9 @@
 const fs = require('fs');
-const input = fs
-  .readFileSync('/dev/stdin')
-  .toString()
-  .trim()
-  .split('\n')
-  .map(Number);
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
 
-const mult = input.reduce((acc, cur) => acc * cur);
+const num = input.reduce((acc, cur) => acc * cur).toString();
 const result = new Array(10).fill(0);
 
-for (const c of mult.toString()) result[c.charCodeAt() - 48]++;
+for (const i in num) result[num[i]]++;
 
 console.log(...result);
