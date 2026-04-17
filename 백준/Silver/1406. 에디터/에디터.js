74b269024de4646f86b276b3655620dbc9ae97a3
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:16:30 +0900

    [Silver II] Title: 에디터, Time: 392 ms, Memory: 83684 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.js
+++ 백준/Silver/1406. 에디터/에디터.js
@@ -1,25 +1,16 @@
 const fs = require('fs');
-const [str, n, ...cmd] = fs
-  .readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const command = cmd.map((line) => line.trim().split(' '));
-const leftStack = str.trim().split('');
+const leftStack = [...input[0].trim()];
 const rightStack = [];
 
-for (const line of command) {
-  switch (line[0]) {
-    case 'L':
-      leftStack.length != 0 && rightStack.push(leftStack.pop());
-      break;
-    case 'D':
-      rightStack.length != 0 && leftStack.push(rightStack.pop());
-      break;
-    case 'B':
-      leftStack.length != 0 && leftStack.pop();
-      break;
-    case 'P':
-      leftStack.push(line[1]);
-      break;
-  }
+for (let i = 2; i < input.length; i++) {
+  if (input[i][0] == 'L')
+    leftStack.length != 0 && rightStack.push(leftStack.pop());
+  else if (input[i][0] == 'D')
+    rightStack.length != 0 && leftStack.push(rightStack.pop());
+  else if (input[i][0] == 'B') leftStack.pop();
+  else if (input[i][0] == 'P') leftStack.push(input[i][2]);
 }
+
 console.log(leftStack.join('') + rightStack.reverse().join(''));
