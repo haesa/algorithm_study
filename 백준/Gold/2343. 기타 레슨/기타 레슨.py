1b249ff09993524e834b62bfa7f6b641d1a492cc
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:30:40 +0900

    [Gold V] Title: 기타 레슨, Time: 276 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.py
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.py
@@ -5,32 +5,31 @@ n, m = map(int, input().split())
 lectures = list(map(int, input().split()))
 
 # 블루레이 개수 계산
-def fn(bluelay_size):
-  num = 0
+def is_valid_blueray_count(lectures, blueray_size, m):
+  if max(lectures) > blueray_size:
+    return False
+  
   sum = 0
-  
-  if max(lectures) > bluelay_size:
-    return 0
-  
+  num = 0
   for size in lectures:
-    if sum + size > bluelay_size:
+    if sum + size > blueray_size:
       num += 1
       sum = 0
     sum += size
 
-  return num + 1
+  return (num + 1) <= m
 
-def binary_search(array):
-  start, end = max(array), sum(array)
+def binary_search(lectures, m):
+  start, end = max(lectures), sum(lectures)
   
   while start <= end:
     mid = (start + end) // 2
     
-    if fn(mid) <= m:
+    if is_valid_blueray_count(lectures, mid, m):
       end = mid - 1
     else:
       start = mid + 1
     
   return start
 
-print(binary_search(lectures))
+print(binary_search(lectures, m))
