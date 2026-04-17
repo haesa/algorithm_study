f0ef3edbee70bec58270895e40d9b7b9366978cd
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:30:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
@@ -8,14 +8,15 @@ const input = fs
 
 input.sort((a, b) => a - b);
 
-const sub = input.reduce((acc, cur) => acc + cur, 0) - 100;
+const solution = (arr) => {
+  const sub = arr.reduce((acc, cur) => acc + cur, 0) - 100;
 
-for (let i = 0; i < 8; i++)
-  for (let j = i + 1; j < 9; j++)
-    if (input[i] + input[j] == sub) {
-      const result = input
-        .filter((height) => height != input[i] && height != input[j])
-        .join('\n');
-      console.log(result);
-      return 0;
-    }
+  for (let i = 0; i < 8; i++)
+    for (let j = i + 1; j < 9; j++)
+      if (arr[i] + arr[j] == sub) {
+        arr.splice(j, 1);
+        arr.splice(i, 1);
+        return arr;
+      }
+};
+console.log(solution(input).join('\n'));
