eb8287250c7ad6240cfc30206328330b43c363f8
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:39:06 +0900

    [Gold IV] Title: 빙산, Time: 684 ms, Memory: 46252 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.js
+++ 백준/Gold/2573. 빙산/빙산.js
@@ -1,100 +1,108 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const [n, m] = input.shift().split(' ').map(Number);
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
 
 class Node {
-  constructor(data, next = null) {
-    this.data = data;
-    this.next = next;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.next = null;
   }
 }
-
 class Queue {
   constructor() {
-    this.head = new Node([]);
-    this.tail = this.head;
+    this.head = null;
+    this.tail = null;
     this.size = 0;
   }
-
-  push(data) {
-    const node = new Node(data);
-    this.tail.next = node;
-    this.tail = node;
-    this.size++;
-  }
-
-  shift() {
-    if (this.size === 0) return;
-    const node = this.head.next;
-    if (node === this.tail) this.tail = this.head;
-    this.head.next = node.next;
-    this.size--;
-    return node.data;
-  }
-
-  getSize() {
+  length() {
     return this.size;
   }
+  push(x, y) {
+    let node = new Node(x, y);
+    if (this.size === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
+  pop() {
+    let temp = this.head;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+    }
+    this.size--;
+    return temp;
+  }
 }
+function sol(map, map2, i, j, visited) {
+  let dx = [0, 0, -1, 1];
+  let dy = [-1, 1, 0, 0];
 
-const map = [];
-const map2 = [];
-for (let i = 0; i < n; i++) {
-  map.push(input[i].split(' ').map(Number));
-  map2.push(input[i].split(' ').map(Number));
-}
-
-function isRange(x, y) {
-  return x >= 0 && x < n && y >= 0 && y < m;
-}
-
-function bfs(i, j, visit) {
-  const dx = [-1, 1, 0, 0];
-  const dy = [0, 0, -1, 1];
-
-  const queue = new Queue();
-  queue.push([i, j]);
-  visit[i][j] = 1;
-  while (queue.getSize()) {
-    const [x, y] = queue.shift();
-    for (let k = 0; k < 4; k++) {
-      const nx = x + dx[k];
-      const ny = y + dy[k];
-      if (!isRange(nx, ny) || visit[nx][ny]) continue;
-      if (map[nx][ny] === 0) map2[x][y]--;
+  let q = new Queue() // x,y
+  q.push(j, i);
+  visited[i][j] = true;
+  while (q.length()) {
+    let cur = q.pop();
+    let [cx, cy] = [cur.x, cur.y];
+    for (let next = 0; next < 4; next++) {
+      let [nx, ny] = [cx + dx[next], cy + dy[next]];
+      if (nx >= M || nx < 0 || ny >= N || ny < 0) continue;
       else {
-        queue.push([nx, ny]);
-        visit[nx][ny] = 1;
+        if (!visited[ny][nx] && map[ny][nx]) {
+          q.push(nx, ny);
+          visited[ny][nx] = true;
+        }
+        if (map[ny][nx] === 0) { // 주변이 바다면 빙하가 녹는다.
+          map2[cy][cx]--;
+          if (map2[cy][cx] < 0) map2[cy][cx] = 0; // 높이는 0보다 줄어들지 않는다.
+        }
       }
     }
-    if (map2[x][y] < 0) map2[x][y] = 0;
   }
 }
+function main() {
+  let answer = 0;
 
-function solution() {
-  const visit = new Array(n);
-  let year = 0;
-
-  while (true) {
-    let count = 0;
-    for (let i = 0; i < n; i++) visit[i] = Array.from({ length: m }, () => 0);
-
-    for (let i = 1; i < n - 1; i++)
-      for (let j = 1; j < m - 1; j++) {
-        if (map[i][j] === 0 || visit[i][j]) continue;
-        bfs(i, j, visit);
-        count++;
-      }
-    if (count > 1) return year;
-
-    let melt = true;
-    for (let i = 1; i < n - 1; i++)
-      for (let j = 1; j < m - 1; j++) {
-        map[i][j] = map2[i][j];
-        if (map[i][j] !== 0) melt = false;
-      }
-    if (melt) return 0;
-    year++;
+  let map = [];
+  let map2 = [];
+  for (let i = 1; i <= N; i++) {
+    map.push(input[i].split(' ').map(Number)); // 원본.
+    map2.push(input[i].split(' ').map(Number)); // 업데이트용.
   }
+
+  let cnt = 0; // 빙산이 몇조각인지.
+  while (1) {
+    let visited = new Array(N).fill(null).map(_ => new Array(M).fill(false));
+    cnt = 0;
+    for (let i = 1; i < N - 1; i++) {
+      for (let j = 1; j < M - 1; j++) {
+        if (map[i][j] !== 0 && !visited[i][j]) {
+          sol(map, map2, i, j, visited);
+          cnt++;
+        }
+      }
+    }
+    if (cnt >= 2) {
+      //console.log(map2)
+      break;
+    } // 빙산이 분리됐을경우 종료.
+
+    let check = true; // 빙산이 다 녹았을 경우.
+    for (let y = 1; y < N - 1; y++) { // 빙하가 녹은 정보를 업데이트한다.
+      for (let x = 1; x < M - 1; x++) {
+        map[y][x] = map2[y][x];
+        if (map[y][x] !== 0) check = false;
+      }
+    }
+    if (check) return console.log(0); // 얼음이 다 녹았을 경우.
+    answer++;
+  }
+  console.log(answer);
 }
-console.log(solution());
+main();
