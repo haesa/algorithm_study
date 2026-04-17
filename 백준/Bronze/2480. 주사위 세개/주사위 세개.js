76a0efc8279a0e7f4653053329ccd18828718c8a
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:33:28 +0900

    [Bronze IV] Title: 주사위 세개, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -1,29 +1,10 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
-
-const dice = new Array(6).fill(0);
-for (let i = 0; i < 3; i++) dice[input[i] - 1]++;
+const [num1, num2, num3] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number).sort((a, b) => a - b);
 
 let answer = 0;
-for (let i = 0; i < 6; i++) {
-  if (dice[i] === 3) {
-    answer = 10000 + (i + 1) * 1000;
-    break;
-  } else if (dice[i] === 2) {
-    answer = 1000 + (i + 1) * 100;
-    break;
-  }
-}
-if (answer === 0) {
-  let count = 0;
-  for (let i = 0; i < 6; i++) {
-    if (dice[i] !== 1) continue;
-    count++;
-    if (count === 3) {
-      answer = (i + 1) * 100;
-      break;
-    }
-  }
-}
+
+if (num1 === num3) answer = 10000 + num1 * 1000;
+else if (num1 === num2 || num2 === num3) answer = 1000 + num2 * 100;
+else answer = num3 * 100;
 
 console.log(answer);
