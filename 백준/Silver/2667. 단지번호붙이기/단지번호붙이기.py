59435c7e90339390044494949aa228319c384e11
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:45:07 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,6 +1,6 @@
 
 # 입력: N, N 줄에 걸쳐 N개의 자료 (지도의 크기, 지도)
-# 로직: BFS로 지도를 탐색하면서 단지수 카운트
+# 로직: DFS로 지도 전체를 탐색하면서 단지수 카운트
 # 출력: 총 단지수와 각 단지에 속하는 집의 수를 오름차순으로 출력
 
 import sys
@@ -18,7 +18,32 @@ for _ in range(n):
 visited = [[0] * n for _ in range(n)]
 d_list = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
-# dfs
+def valid_boundary(r, c):
+  return r >= 0 and r < n and c >= 0 and c < n
+
+#dfs
+def dfs(graph, r, c):
+  count = 0
+  stack = [[r, c]]
+  
+  while len(stack) > 0:
+    current_r, current_c = stack.pop()
+    
+    if visited[current_r][current_c] or graph[current_r][current_c] == 0:
+      continue
+    
+    visited[current_r][current_c] = 1
+    count += 1
+    
+    for dr, dc in d_list:
+      next_r, next_c = current_r + dr, current_c + dc
+      
+      if valid_boundary(next_r, next_c):
+        stack.append([next_r, next_c])
+
+  return count
+  
+# bfs
 def bfs(graph, r, c):
   count = 0
   queue = deque([[r, c]])
@@ -39,16 +64,13 @@ def bfs(graph, r, c):
         queue.append([next_r, next_c])
 
   return count
-  
-def valid_boundary(r, c):
-  return r >= 0 and r < n and c >= 0 and c < n
 
 # 출력
 result = []
 for r in range(n):
   for c in range(n):
     if map_list[r][c] == 1 and visited[r][c] == 0:
-      result.append(bfs(map_list, r, c))
+      result.append(dfs(map_list, r, c))
 
 result.sort()
 
