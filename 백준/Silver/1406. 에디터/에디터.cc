c4c4180c9511a739cfce4661a9f7f98a751835c6
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:16:19 +0900

    [Silver II] Title: 에디터, Time: 72 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -5,9 +5,13 @@ using namespace std;
 list<char> l;
 list<char>::iterator t;
 string s;
-string cmd;
 int n;
+char cmd;
+
 int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
   cin >> s;
   for (char c : s) l.push_back(c);
   t = l.end();
@@ -15,28 +19,25 @@ int main() {
   cin.ignore();
 
   for (int i = 0; i < n; i++) {
-    getline(cin, cmd);
-    for (int j = 0; j < cmd.length(); j++) {
-      switch (cmd[j]) {
-        case 'L':
-          if (t != l.begin()) t--;
-          break;
-        case 'D':
-          if (t != l.end()) t++;
-          break;
-        case 'B':
-          if (t == l.begin()) continue;
-          if (t == l.end()) {
-            t = l.erase(--t);
-            t = l.end();
-          } else
-            t = l.erase(--t);
-          break;
-        case 'P':
-          while (cmd[++j] == ' ')
-            ;
-          l.insert(t, cmd[j]);
-      }
+    cin >> cmd;
+    switch (cmd) {
+      case 'L':
+        if (t != l.begin()) t--;
+        break;
+      case 'D':
+        if (t != l.end()) t++;
+        break;
+      case 'B':
+        if (t == l.begin()) continue;
+        if (t == l.end()) {
+          t = l.erase(--t);
+          t = l.end();
+        } else
+          t = l.erase(--t);
+        break;
+      case 'P':
+        cin >> cmd;
+        l.insert(t, cmd);
     }
   }
   for (auto i : l) cout << i;
