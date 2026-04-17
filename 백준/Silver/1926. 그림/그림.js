78b21e15508bb2447081cb324296f57dd02774bb
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:23:23 +0900

    [Silver I] Title: 그림, Time: 296 ms, Memory: 23792 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.js
+++ 백준/Silver/1926. 그림/그림.js
@@ -1,25 +1,25 @@
 const [nm, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const [n, m] = nm.split(' ').map(Number);
 const picture = input.map((s) => s.split(' ').map(Number));
-
+const visit = Array.from({length: n}, () => Array.from({length: m}, () => 0));
 const dx = [1, 0, -1, 0];
 const dy = [0, 1, 0, -1];
 
 function bfs (i, j) {
     const queue = [[i, j]];
-    picture[i][j] = 0;
     let area = 0;
+    visit[i][j] = 1;
     while(queue.length > 0){
         const [x, y] = queue.shift();
         area += 1;
         for(let k = 0; k < 4; k++){
             const nx = x + dx[k];
             const ny = y + dy[k];
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m || picture[nx][ny] === 0){
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m || picture[nx][ny] === 0 || visit[nx][ny] === 1) {
                 continue;
             }
             queue.push([nx, ny]);
-            picture[nx][ny] = 0;
+            visit[nx][ny] = 1;
         }
     }
     return area;
@@ -30,7 +30,7 @@ function solution() {
     let max = 0;
     for(let i = 0; i < n; i++) {
         for(let j = 0; j < m; j++) {
-            if(picture[i][j] === 0) {
+            if(picture[i][j] === 0 || visit[i][j] === 1) {
                 continue;
             }
             const size = bfs(i, j);
