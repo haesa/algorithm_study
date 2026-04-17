dd3765e3260697ee2fbe1d84f16cc05f02d171f7
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:29:52 +0900

    [Gold V] Title: 동전 2, Time: 592 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -2,15 +2,14 @@ import sys
 read = sys.stdin.readline
 
 N, K = map(int, read().split())
-coins = list(dict.fromkeys([int(read()) for _ in range(N)]))
-
+coins = [int(read().rstrip()) for _ in range(N)]
 DP = [float('inf')] * (K + 1)
-for coin in coins:
+
+for coin in reversed(coins):
     if coin <= K:
         DP[coin] = 1
-for i in range(1, K + 1):
-    for coin in coins:
-        if i > coin:
+    for i in range(1, K + 1):
+        if i - coin > 0 and not DP[i - coin] == float('inf'):
             DP[i] = min(DP[i], DP[i - coin] + 1)
 
 print(-1 if DP[K] == float('inf') else DP[K])
