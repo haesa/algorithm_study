2ec052846e3992ed2e4e636241bff17eb5cfa65e
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:45:09 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,79 +1,46 @@
-
-# 입력: N, N 줄에 걸쳐 N개의 자료 (지도의 크기, 지도)
-# 로직: DFS로 지도 전체를 탐색하면서 단지수 카운트
-# 출력: 총 단지수와 각 단지에 속하는 집의 수를 오름차순으로 출력
-
 import sys
 from collections import deque
+read = sys.stdin.readline
 
-input = sys.stdin.readline
+N = int(read().strip())
+graph = [list(map(int, read().rstrip())) for _ in range(N)]
 
-# 입력
-n = int(input())
+visitted = [[0] * N for _ in range(N)]
 
-map_list = []
-for _ in range(n):
-  map_list.append(list(map(int, input().strip())))
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
 
-visited = [[0] * n for _ in range(n)]
-d_list = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
 def valid_boundary(r, c):
-  return r >= 0 and r < n and c >= 0 and c < n
+    return r >= 0 and r < N and c >= 0 and c < N
 
-#dfs
-def dfs(graph, r, c):
-  count = 0
-  stack = [[r, c]]
-  
-  while len(stack) > 0:
-    current_r, current_c = stack.pop()
-    
-    if visited[current_r][current_c] or graph[current_r][current_c] == 0:
-      continue
-    
-    visited[current_r][current_c] = 1
-    count += 1
-    
-    for dr, dc in d_list:
-      next_r, next_c = current_r + dr, current_c + dc
-      
-      if valid_boundary(next_r, next_c):
-        stack.append([next_r, next_c])
 
-  return count
-  
-# bfs
-def bfs(graph, r, c):
-  count = 0
-  queue = deque([[r, c]])
-  
-  while len(queue) > 0:
-    current_r, current_c = queue.popleft()
-    
-    if visited[current_r][current_c] or graph[current_r][current_c] == 0:
-      continue
-    
-    visited[current_r][current_c] = 1
-    count += 1
-    
-    for dr, dc in d_list:
-      next_r, next_c = current_r + dr, current_c + dc
-      
-      if valid_boundary(next_r, next_c):
-        queue.append([next_r, next_c])
+def bfs(start_r, start_c):
+    q = deque([(start_r, start_c)])
+    visitted[start_r][start_c] = 1
+    count = 1
 
-  return count
+    while q:
+        r, c = q.popleft()
 
-# 출력
-result = []
-for r in range(n):
-  for c in range(n):
-    if map_list[r][c] == 1 and visited[r][c] == 0:
-      result.append(dfs(map_list, r, c))
+        for i in range(4):
+            nr, nc = r + dr[i], c + dc[i]
 
-result.sort()
+            if valid_boundary(nr, nc) and graph[nr][nc] and not visitted[nr][nc]:
+                q.append((nr, nc))
+                visitted[nr][nc] = 1
+                count += 1
+    return count
 
-print(len(result))
-for answer in result:
-  print(answer)
+
+answer = []
+count = 0
+for i in range(N):
+    for j in range(N):
+        if not graph[i][j]:
+            continue
+        if not visitted[i][j]:
+            answer.append(bfs(i, j))
+            count += 1
+
+print(count, *sorted(answer), sep='\n')
