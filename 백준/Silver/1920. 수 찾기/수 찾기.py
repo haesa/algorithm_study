10cda4f634e6e3dada9d1d8839de460db2df4993
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:22:50 +0900

    [Silver IV] Title: 수 찾기, Time: 116 ms, Memory: 52432 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -4,31 +4,11 @@ input = sys.stdin.readline
 write = sys.stdout.write
 
 N = int(input())
-A = list(map(int, input().split()))
+A = set(map(int, input().split()))
 M = int(input())
 B = list(map(int, input().split()))
 
-# 이진 탐색
-def binary_search(list, target):
-  start = 0
-  end = len(list) - 1
-  
-  while start <= end:
-    mid = (start + end) // 2
-    
-    if list[mid] > target:
-      end = mid - 1
-    elif list[mid] < target:
-      start = mid + 1
-    else:
-      return 1
-  
-  return 0
-
-A.sort()
-
 result = []
 for num in B:
-  result.append(binary_search(A, num))
-
-write('\n'.join(map(str, result)))
+    result.append('1' if num in A else '0')
+write('\n'.join(result))
