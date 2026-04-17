16663cfa9483789fce8efa981375e7c32a133dbe
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:38:21 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,12 +1,6 @@
-const fs = require('fs');
-const input = fs
-  .readFileSync('/dev/stdin')
-  .toString()
-  .trim()
-  .split('\n')
-  .map(Number);
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(Number);
 
-let max = 0;
-let index;
-input.forEach((num, i) => num > max && ((max = num), (index = i + 1)));
-console.log(`${max}\n${index}`);
+let max = Math.max(...input)
+
+console.log(max)
+console.log(input.indexOf(max) + 1)
