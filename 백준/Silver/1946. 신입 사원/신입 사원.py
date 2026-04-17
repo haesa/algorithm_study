5bf979017bc3069ea532581a4d035311faff63f0
haesa <34948133+haesa@users.noreply.github.com>
2026-04-17 10:24:34 +0900

    [Silver I] Title: 신입 사원, Time: 5604 ms, Memory: 73420 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,14 +1,19 @@
 import sys
 
-t = int(sys.stdin.readline())
-for _ in range(t):
-  n = int(sys.stdin.readline())
-  scores = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
-  scores.sort()
-  result = 1
-  min_value = scores[0][1]
-  for i in range(1, n):
-    if min_value >= scores[i][1]: 
-      result += 1
-      min_value = scores[i][1]
-  print(result)
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+  N = int(input())
+  rank_list = [list(map(int, input().split())) for _ in range(N)]
+  rank_list.sort() # 서류심사 순위로 정렬
+  count = 1 # 선발된 지원자의 수
+  
+  highest_rank = rank_list[0][1] # 서류심사 1등의 면접심사 순위
+  for i in range(N):
+    if rank_list[i][1] < highest_rank: # 서류심사 순위가 더 높은 지원자들의 면접심사 순위보다 더 높으면 합격
+      count += 1
+      highest_rank = rank_list[i][1]
+  
+  print(count)  # 선발되 지원자 수 출력
