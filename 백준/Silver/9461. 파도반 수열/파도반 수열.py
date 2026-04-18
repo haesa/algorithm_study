83c79aabc0cbfffe1524184378dd19526f1cac6a
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 17:52:16 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -2,20 +2,16 @@ import sys
 read = sys.stdin.readline
 
 T = int(read())
-
-for t in range(T):
-    P = int(read())
-
-    DP = [0] * (P + 1)
-    if 1 <= P <= 3:
+for _ in range(T):
+    N = int(read())
+    if 1 <= N <= 3:
         print(1)
-        continue
-    elif 4 <= P <= 5:
+    elif 4 <= N <= 5:
         print(2)
-        continue
     else:
+        DP = [0] * (N + 1)
         DP[1] = DP[2] = DP[3] = 1
         DP[4] = DP[5] = 2
-        for i in range(6, P + 1):
+        for i in range(6, N + 1):
             DP[i] = DP[i - 1] + DP[i - 5]
-        print(DP[P])
+        print(DP[N])
