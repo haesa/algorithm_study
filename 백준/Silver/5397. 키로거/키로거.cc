2a80bd1dbc3b59fedf4031d772a7f58305d2be57
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 17:43:14 +0900

    [Silver II] Title: 키로거, Time: 212 ms, Memory: 18740 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+string input;
+
+int n;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> input;
+    list<char> l;
+    list<char>::iterator t = l.end();
+    for (char c : input) {
+      switch (c) {
+        case '<':
+          if (t == l.begin()) continue;
+          t--;
+          break;
+        case '>':
+          if (t == l.end()) continue;
+          t++;
+          break;
+        case '-':
+          if (t == l.begin()) continue;
+          if (t == l.end()) {
+            l.erase(--t);
+            t = l.end();
+          } else
+            t = l.erase(--t);
+          break;
+        default:
+          l.insert(t, c);
+      }
+    }
+    for (char c : l) cout << c;
+    cout << '\n';
+  }
+}
