5dd04dd525a2d0377fc4c6ee7d39ebdb74bdcf90
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 17:40:00 +0900

    [Silver III] Title: 두 수의 합, Time: 220 ms, Memory: 18416 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.js
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const arr = input[1].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number).sort((a, b) => a - b);
 const x = Number(input[2]);
 
-const check = new Array(1000001).fill(0);
-let result = 0;
-arr.forEach((num) => {
-  check[x - num] && result++;
-  check[num] = 1;
-});
+let left = 0;
+let right = arr.length - 1;
+let cnt = 0;
 
-console.log(result);
+while (left < right) {
+  if (arr[left] + arr[right] == x) {
+    cnt++;
+    left++;
+  } else if (arr[left] + arr[right] > x) right--;
+  else left++;
+}
+
+console.log(cnt);
