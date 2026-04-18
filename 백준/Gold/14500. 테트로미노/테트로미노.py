49bc0a7b56426580e130ed288379c1a7a6811b91
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:45:13 +0900

    [Gold IV] Title: 테트로미노, Time: 6132 ms, Memory: 39904 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -9,7 +9,7 @@ board = [list(map(int, input().split())) for _ in range(N)]
 
 """
 DFS 탐색(백트래킹)으로 4칸을 이동하여 테트로미노 도형 방문 체크
-산모양 테트로미노는 중간에서 분기하는 형태이므로 별도 계산
+'ㅗ' 테트로미노는 날개 4개를 더하고 가장 작은 값을 빼서 계산
 """
 
 # 상 우 하 좌
@@ -17,51 +17,33 @@ d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 visited = [[0] * M for _ in range(N)]
 
+result = 0
 
-def dfs(r, c, size):
+
+def dfs(r, c, size, total):
+    global result
+
     if size == 4:
-        return board[r][c]
-
-    visited[r][c] = 1
-    max_value = 0
+        result = max(total, result)
+        return
 
     for dr, dc in d:
         nr, nc = r + dr, c + dc
 
         if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc]:
-            max_value = max(max_value, dfs(nr, nc, size + 1))
+            visited[nr][nc] = 1
 
-    visited[r][c] = 0
+            if size == 2:
+                dfs(r, c, size + 1, total + board[nr][nc])
 
-    return max_value + board[r][c]
-
-
-def calc(r, c):
-    value = board[r][c]
-    min_value = INF
-    count = 0
-
-    for i in range(4):
-        nr, nc = r + d[i][0], c + d[i][1]
-
-        if 0 <= nr < N and 0 <= nc < M:
-            value += board[nr][nc]
-            min_value = min(board[nr][nc], min_value)
-            count += 1
-
-    if count == 3:
-        return value
-
-    if count == 4:
-        return value - min_value
-
-    return 0
+            dfs(nr, nc, size + 1, total + board[nr][nc])
+            visited[nr][nc] = 0
 
 
-result = 0
 for i in range(N):
     for j in range(M):
-        result = max(result, dfs(i, j, 1))
-        result = max(result, calc(i, j))
+        visited[i][j] = 1
+        dfs(i, j, 1, board[i][j])
+        visited[i][j] = 0
 
 print(result)
