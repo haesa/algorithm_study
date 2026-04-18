9456ae809a7775dffa8f0bd48a8edfe94111aeaa
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 17:53:08 +0900

    [Silver I] Title: 완전 이진 트리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -1,48 +1,24 @@
+# 완전 이진 트리가 아니라 포화 이진 트리 문제 같은데? ㅇㅅㅇ
 import sys
-from collections import deque
 
 read = sys.stdin.readline
 
 K = int(read())
 inorder = list(map(int, read().split()))
+result = [[] for _ in range(K)]
 
 
-def bfs():
-    result = []
-    q = deque([(K, 0, len(inorder) - 1)])
+def dfs(graph, k):
+    n = len(graph)
+    if n < 1:
+        return
+    mid = n // 2
+    result[k].append(graph[mid])
 
-    while q:
-        k, start, end = q.popleft()
-
-        # 서브트리가 없는 경우 다음 반복으로 이동
-        if start > end:
-            continue
-
-        full_tree_size = 2**k - 1  # 완전 이진 트리가 꽉 차 있는 경우 크기
-        tree_size = end - start + 1  # 현재 트리의 크기
-
-        # 마지막 레벨에서 노드가 절반이상 찼는지 확인, 그에 따라 왼쪽 서브트리의 크기를 결정
-        left_size = 2**(k-1) - 1 if tree_size >= full_tree_size - \
-            2**(k-2) else tree_size - 2**(k-2)
-        # inorder에서의 루트의 위치 확인
-        root_pos = start + left_size
-        # 루트 노드 result에 추가
-        result.append(inorder[root_pos])
-
-        # 마지막 레벨에서 노드가 절반을 넘었는지 확인, 그에 따라 오른쪽 서브트리의 k를 결정
-        right_next_k = k - 1 if tree_size > full_tree_size - \
-            2**(k-2) else k - 2
-
-        q.append((k - 1, start, root_pos - 1))
-        q.append((right_next_k, root_pos + 1, end))
-
-    return result
+    dfs(graph[:mid], k + 1)
+    dfs(graph[mid + 1:], k + 1)
 
 
-result = bfs()
-i = 0
-for k in range(1, K + 1):
-    while i < 2**(k) - 1:
-        print(result[i], end=' ')
-        i += 1
-    print('')
+dfs(inorder, 0)
+for level in result:
+    print(*level)
