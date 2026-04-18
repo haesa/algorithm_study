903ed1639299c606e19468a1e76d9a0b82643d8f
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 17:43:58 +0900

    [Silver II] Title: 키로거, Time: 644 ms, Memory: 145152 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.js
+++ 백준/Silver/5397. 키로거/키로거.js
@@ -1,26 +1,29 @@
 const fs = require('fs');
-// const input = fs.readFileSync('./input/5397.txt').toString().trim().split('\n');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const N = +input.shift();
 
-let left = [];
-let right = [];
+const n = Number(input[0]);
+let leftStack = [];
+let rightStack = [];
 
-for(let i = 0; i < N; i++) {
-	const pw = input[i];
-	for(let j = 0; j < pw.length; j++) {
-		const letter = pw[j];
-		if (letter === '-' && left.length != 0) {
-			left.pop();
-		} else if (letter === '<' && left.length != 0) {
-			right.push(left.pop());
-		} else if (letter === '>' && right.length != 0) {
-			left.push(right.pop());
-		} else if (letter !== '-' && letter !== '<' && letter !== '>') {
-			left.push(letter);
-		}
-	}
-	console.log(left.join('')+right.reverse().join(''));
-	left = [];
-	right = [];
+for (let i = 1; i <= n; i++) {
+  const pw = input[i];
+
+  for (const c of pw) {
+    switch (c) {
+      case '<':
+        leftStack.length && rightStack.push(leftStack.pop());
+        break;
+      case '>':
+        rightStack.length && leftStack.push(rightStack.pop());
+        break;
+      case '-':
+        leftStack.pop();
+        break;
+      default:
+        leftStack.push(c);
+    }
+  }
+  console.log(leftStack.join('') + rightStack.reverse().join(''));
+  leftStack = [];
+  rightStack = [];
 }
