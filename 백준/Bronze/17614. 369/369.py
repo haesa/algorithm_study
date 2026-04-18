709cc775b30a9402c014b66bc9f4b8cb18577d8b
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:56:25 +0900

    [Bronze III] Title: 369, Time: 692 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -2,8 +2,7 @@ n = int(input())
 
 result = 0
 for num in range(1, n + 1):
-  for c in str(num):
-    if c == '3' or c == '6' or c == '9':
-      result += 1
-      
+  num_str = str(num)
+  result += num_str.count('3') + num_str.count('6') + num_str.count('9')
+
 print(result)
