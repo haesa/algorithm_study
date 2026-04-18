ebadc6befdffcdf16f08e4654fc0ebea07743880
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 17:50:30 +0900

    [Silver V] Title: 올림픽, Time: 32 ms, Memory: 31120 KB, Score: 8 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -9,10 +9,10 @@ order = sorted(prize.items(), key = lambda x : (-x[1][0], -x[1][1], -x[1][2]))
 
 rank = [0] * (n + 1)
 score = order[0][1]
-rank_num = 1
-for code, medals in order:
+rank_num = 0
+for index, (code, medals) in enumerate(order):
   if medals != score:
-    rank_num += 1    
+    rank_num = index + 1
   rank[code] = rank_num
   score = medals
 
