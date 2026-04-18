3073dfb35139ab28368999aef580c9c6ea52ca46
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 12:34:22 +0900

    [Silver IV] Title: 암기왕, Time: 5772 ms, Memory: 226196 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,16 +1,27 @@
+def binary_search(list, target):
+  left, right = 0, len(list) - 1
+  
+  while left <= right:
+    mid = (left + right) // 2
+    if list[mid] == target:
+      return 1
+    elif list[mid] < target:
+      left = mid + 1
+    else:
+      right = mid - 1
+  
+  return 0
+  
 t = int(input())
 
 for i in range(t):
   n = input()
   n_input = input()
-  n_set = set(map(int, n_input.split(" ")))
+  n_list = sorted(map(int, n_input.split(" ")))
 
   m = input()
   m_input = input()
   m_list = list(map(int, m_input.split(" ")))
 
   for num in m_list:
-    if num in n_set:
-      print(1)
-    else:
-      print(0)
+    print(binary_search(n_list, num))
