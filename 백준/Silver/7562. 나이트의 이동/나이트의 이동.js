72391a69afc6a9785017c56098ef5f022ccde43d
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 17:47:32 +0900

    [Silver I] Title: 나이트의 이동, Time: 628 ms, Memory: 30236 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.js
@@ -0,0 +1,47 @@
+const [t, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const dx = [-2, -2, 2, 2, -1, 1, -1, 1];
+const dy = [-1, 1, -1, 1, 2, 2, -2, -2];
+
+let l;
+let map;
+function mapping() {
+  map = new Array(l);
+  for (let i = 0; i < l; i++) map[i] = Array.from({ length: l }, () => 0);
+}
+
+function isVisit(x, y) {
+  return map[x][y];
+}
+
+function isRange(x, y) {
+  return x >= 0 && x < l && y >= 0 && y < l;
+}
+
+function bfs([a, b]) {
+  const queue = [[a, b, 0]];
+  map[a][b] = 1;
+  [endX, endY] = input.shift().split(' ').map(Number);
+
+  while (true) {
+    const [x, y, d] = queue.shift();
+    if (x === endX && y === endY) return d;
+    for (let k = 0; k < 8; k++) {
+      const nx = x + dx[k];
+      const ny = y + dy[k];
+      if (!isRange(nx, ny) || isVisit(nx, ny)) continue;
+      queue.push([nx, ny, d + 1]);
+      map[nx][ny] = 1;
+    }
+  }
+}
+
+function solution() {
+  for (let i = 0; i < t; i++) {
+    l = Number(input.shift());
+    mapping();
+    start = input.shift().split(' ').map(Number);
+    console.log(bfs(start));
+  }
+}
+solution();
