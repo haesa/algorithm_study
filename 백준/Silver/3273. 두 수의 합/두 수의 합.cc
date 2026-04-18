44cf12292be064ce7280ccc5ca038f872580ff9b
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 17:39:49 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 10224 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -2,18 +2,18 @@
 #include <iostream>
 using namespace std;
 int check[2000001] = {};
+int a[100000];
+int n, x, result = 0;
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int n, x, result = 0;
-  int a[100000];
+
   cin >> n;
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
-    check[a[i]]++;
-  }
+  for (int i = 0; i < n; i++) cin >> a[i];
   cin >> x;
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++) {
     if ((x > a[i]) && check[x - a[i]]) result++;
-  cout << result / 2;
+    check[a[i]] = 1;
+  }
+  cout << result;
 }
