a2b15a596e06e7c85f29a0cca5f266e42d82e525
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:45:03 +0900

    [Gold IV] Title: 테트로미노, Time: 3784 ms, Memory: 39912 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -2,14 +2,13 @@ import sys
 
 input = sys.stdin.readline
 
-INF = float("inf")
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-MAX = max(map(max, board))
 
 """
 DFS 탐색(백트래킹)으로 4칸을 이동하여 테트로미노 도형 방문 체크
+산모양 테트로미노는 중간에서 분기하는 형태이므로 별도 계산
 """
 
 # 상 우 하 좌
@@ -17,36 +16,58 @@ d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 visited = [[0] * M for _ in range(N)]
 
-result = 0
 
-
-def dfs(r, c, size, total):
-    global result
-
-    if total + (4 - size) * MAX < result:
-        return
-
+def dfs(r, c, size):
     if size == 4:
-        result = max(total, result)
-        return
+        return board[r][c]
+
+    visited[r][c] = 1
+    max_value = 0
 
     for dr, dc in d:
         nr, nc = r + dr, c + dc
 
         if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc]:
-            visited[nr][nc] = 1
+            max_value = max(max_value, dfs(nr, nc, size + 1))
 
-            if size == 2:
-                dfs(r, c, size + 1, total + board[nr][nc])
+    visited[r][c] = 0
 
-            dfs(nr, nc, size + 1, total + board[nr][nc])
-            visited[nr][nc] = 0
+    return max_value + board[r][c]
 
 
+def calc(r, c):
+    max_value = 0
+
+    for i in range(4):
+        value = board[r][c]
+
+        nr, nc = r + d[i][0], c + d[i][1]
+        if not (0 <= nr < N and 0 <= nc < M):
+            continue
+
+        value += board[nr][nc]
+
+        # 분기
+        base_r, base_c = nr, nc
+        for di in [-1, 1]:
+            ni = (i + di + 4) % 4
+            nr = base_r + d[ni][0]
+            nc = base_c + d[ni][1]
+
+            if not (0 <= nr < N and 0 <= nc < M):
+                break
+
+            value += board[nr][nc]
+
+        max_value = max(value, max_value)
+
+    return max_value
+
+
+result = 0
 for i in range(N):
     for j in range(M):
-        visited[i][j] = 1
-        dfs(i, j, 1, board[i][j])
-        visited[i][j] = 0
+        result = max(result, dfs(i, j, 1))
+        result = max(result, calc(i, j))
 
 print(result)
