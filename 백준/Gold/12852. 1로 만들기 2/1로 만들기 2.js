ad8aa7a0c6b2362de68d828baebd1d60e50522c5
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:42:03 +0900

    [Gold V] Title: 1로 만들기 2, Time: 340 ms, Memory: 25824 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.js
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.js
@@ -4,21 +4,28 @@ const d = Array.from({ length: n + 1 }, () => 0);
 const path = Array.from({ length: n + 1 }, () => 0);
 
 d[1] = 0;
-path[1] = [1];
+path[1] = 0;
 
 for(let i = 2; i <= n; i++) {
     d[i] = d[i - 1] + 1;
-    path[i] = [...path[i - 1], i];
+    path[i] = i - 1;
     
     if(i % 3 === 0 && d[i / 3] + 1 < d[i]) {
         d[i] = d[i / 3] + 1;
-        path[i] = [...path[i / 3], i];
+        path[i] = i / 3;
     }
     if(i % 2 === 0 && d[i / 2] + 1 < d[i]) {
         d[i] = d[i / 2] + 1;
-        path[i] = [...path[i / 2], i];
+        path[i] = i / 2;
     }
 }
 
+const pathResult = [];
+let i = n;
+while(i > 0) {
+    pathResult.push(i);
+    i = path[i];
+}
+
 console.log(d[n]);
-console.log([...path[n]].reverse().join(' '));
+console.log(pathResult.join(' '));
