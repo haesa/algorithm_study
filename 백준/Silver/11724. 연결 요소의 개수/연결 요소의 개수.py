412808a204e20ee8a2ff7b1c3dd8c16dded03218
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:33:48 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 612 ms, Memory: 67060 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,44 +1,35 @@
 import sys
+from collections import deque
 
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
 graph = [[] for _ in range(N + 1)]
 visited = [0] * (N + 1)
-
 for _ in range(M):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
 
-# def bfs(v):
-#     q = deque([v])
-#     visited[v] = 1
-
-#     while q:
-#         v = q.popleft()
-
-#         for u in graph[v]:
-#             if not visited[u]:
-#                 q.append(u)
-#                 visited[u] = 1
-
-
-def dfs(v):
+def bfs(v):
+    q = deque([v])
     visited[v] = 1
 
-    for u in graph[v]:
-        if not visited[u]:
-            dfs(u)
+    while q:
+        v = q.popleft()
+
+        for u in graph[v]:
+            if not visited[u]:
+                q.append(u)
+                visited[u] = 1
 
 
 count = 0
 for i in range(1, N + 1):
     if not visited[i]:
-        dfs(i)
+        bfs(i)
         count += 1
 
 print(count)
