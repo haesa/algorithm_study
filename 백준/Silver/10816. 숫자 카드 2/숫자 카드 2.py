22f4e9e76be8a40c6d48d676e21b4eb8347c0e60
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:05:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 660 ms, Memory: 126856 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,21 +1,13 @@
 import sys
-from bisect import bisect_left, bisect_right
+from collections import Counter
 
 input = sys.stdin.readline
 write = sys.stdout.write
 
 N = int(input())
-A = list(map(int, input().split()))
+A = Counter(map(int, input().split()))
 M = int(input())
 B = list(map(int, input().split()))
 
-A.sort()
-
-result = []
 for num in B:
-  pos_left = bisect_left(A, num)
-  pos_right = bisect_right(A, num)
-  
-  result.append(pos_right - pos_left)
-  
-write(' '.join(map(str, result)))
+  write(str(A[num]) + ' ')
