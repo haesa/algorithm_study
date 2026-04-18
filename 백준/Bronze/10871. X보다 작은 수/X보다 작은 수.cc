73aae4e1ba882cb7bb687c2e7110e4fc8dc17f45
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:19:03 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -2,11 +2,13 @@
 using namespace std;
 int main()
 {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
   int n, x, item;
   cin >> n >> x;
   for (int i = 0; i < n; i++)
   {
     cin >> item;
-    if(item < x) cout << item << ' ';
+    (item < x) && cout << item << ' ';
   }
 }
