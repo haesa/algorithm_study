c57d584c9abf57113670008d742c5d965e8bcd38
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:25:17 +0900

    [Bronze II] Title: Strfry, Time: 196 ms, Memory: 12784 KB -BaekjoonHub

---
+++ 백준/Bronze/11328. Strfry/Strfry.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = Number(input[0]);
+let answer = '';
+
+function replaceCheck([str1, str2]) {
+  const alpha1 = new Array(26).fill(0);
+  const alpha2 = new Array(26).fill(0);
+  for (let i = 0; i < str1.length; i++) alpha1[str1.charCodeAt(i) - 97]++;
+  for (let i = 0; i < str2.length; i++) alpha2[str2.charCodeAt(i) - 97]++;
+  for (let i = 0; i < 26; i++)
+    if (alpha1[i] !== alpha2[i]) return 'Impossible ';
+  return 'Possible ';
+}
+
+for (let i = 1; i <= n; i++) {
+  const str = input[i].trim().split(' ');
+  answer += replaceCheck(str);
+}
+console.log(answer);
