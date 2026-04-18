729f68ed51e9c9243e6b520d45c5d15d0e4357cf
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:56:22 +0900

    [Bronze III] Title: 369, Time: 916 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,11 +1,9 @@
-def include369(number_str):
-  return number_str == '3' or number_str == '6' or number_str == '9'
+n = int(input())
 
 result = 0
-n = int(input())
-for i in range(1, n + 1):
-  for c in str(i):
-    if include369(c):
+for num in range(1, n + 1):
+  for c in str(num):
+    if c == '3' or c == '6' or c == '9':
       result += 1
       
 print(result)
