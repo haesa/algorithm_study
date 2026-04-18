9bdac3cd22c6a7759a3460b3cc1720b58935bda0
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 17:56:58 +0900

    [Bronze II] Title: 카드 역배치, Time: 140 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.js
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
-const [...input] = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map((x) => x.split(' ').map(Number));
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map((x) => x.split(' ').map(Number));
 
 const card = new Array(20).fill().map((_, i) => i + 1);
 
 for (let i = 0; i < input.length; i++) {
   const [a, b] = input[i];
-  const subArr = card.slice(a - 1, b).reverse();
-  card.splice(a - 1, b - a + 1, ...subArr);
+  card.splice(a - 1, b - a + 1, ...card.slice(a - 1, b).reverse());
 }
-
 console.log(card.join(' '));
