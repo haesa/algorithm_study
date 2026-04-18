55da66428718f556cba810c1460738feb04102ae
haesa <34948133+haesa@users.noreply.github.com>
2026-04-18 18:44:32 +0900

    [Gold IV] Title: 회사 문화 1, Time: 164 ms, Memory: 43484 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -1,14 +1,8 @@
 import sys
-from collections import deque, defaultdict
 read = sys.stdin.readline
 
 N, M = map(int, read().split())
 supervisor_list = [0, *list(map(int, read().split()))]  # 상사 리스트
-
-# 상사 - 부하 매핑
-sub_dict = defaultdict(list)
-for i in range(1, N + 1):
-    sub_dict[supervisor_list[i]].append(i)
 
 # 직속 상사로부터 받은 칭찬 정도 저장
 answer = [0] * (N + 1)
@@ -16,17 +10,7 @@ for _ in range(M):
     i, w = map(int, read().split())
     answer[i] += w
 
+for i in range(2, N + 1):
+    answer[i] = answer[i] + answer[supervisor_list[i]]
 
-def bfs():
-    q = deque([1])  # 사장부터 시작
-
-    while q:
-        i = q.popleft()
-
-        for sub in sub_dict[i]:  # 부하 직원 탐색
-            q.append(sub)
-            answer[sub] = answer[sub] + answer[i]  # 칭찬 정도 누적합
-
-
-bfs()
 print(*answer[1:])
