4baa90e2737ff360a182ce77544095998579d728
haesa <34948133+haesa@users.noreply.github.com>
2026-09-03 23:44:38 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 0.82 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
@@ -1,43 +1,39 @@
 '''
-출력: 스포 방지 단어 리스트 중 중복 제거하고, 평문(스포 구간 X)에 나온 적 없는 단어의 개수
-필요한 데이터: 스포 방지 단어 리스트, 스포 구간이 아닌 단어 리스트
-
-- 스포 구간 -> 스포 방지 단어 리스트, 스포 구간이 아닌 단어 리스트 구하기
-- 스포 방지 단어 리스트 중복 제거
-- 스포 방지 단어 리스트 순회 -> 스포 구간이 아닌 단어 리스트에 포함되지 않으면 카운트
+출력: 스포 방지 단어 집합에 대한 일반 단어 차집합의 크기
+필요한 데이터: 스포 방지 단어 집합(spoiler), 일반 단어 집합(normal)
 '''
 
 def solution(message, spoiler_ranges):
-    prev_end = 0
-    spoiler_words = []
-    non_spoiler_words = []
-    for start, end in spoiler_ranges:
-        # 스포 방지 단어 리스트
-        while message[start] != ' ' and start > 0:
-            start -= 1
-        while message[end] != ' ' and end < len(message) - 1:
-            end += 1
+    spoiler = set()
+    normal = set()
+    
+    ws = 1 if message[0] == ' ' else 0
+    i = 0
+    
+    while ws < len(message):
+        # [ws, we) 형태로 단어 찾기
+        we = ws + 1
         
-        for word in message[start:end+1].rstrip().split():
-            if word not in spoiler_words:
-                spoiler_words.append(word)
+        while we < len(message) and message[we] != ' ':
+            we += 1
+        word = message[ws:we]
         
-        # 스포 구간이 아닌 단어 리스트
-        for word in message[prev_end:start].rstrip().split():
-            if word not in non_spoiler_words:
-                non_spoiler_words.append(word)
+        # 현재 단어보다 앞에 있는 스포 구간 제거
+        while i < len(spoiler_ranges) and spoiler_ranges[i][1] < ws:
+            i += 1
+            
+        if i < len(spoiler_ranges):
+            # 단어 구간 [ws, we)
+            # 스포 구간 [ss, se]
+            ss, se = spoiler_ranges[i]
+            if se >= ws and we > ss: # 스포일러 구간 겹치는 경우
+                spoiler.add(word)
+            else:
+                normal.add(word)
+        else:
+            normal.add(word)
         
-        prev_end = end
-
-    # 스포일러 구간 뒤에 message가 남은 경우 처리
-    for word in message[prev_end:len(message)].rstrip().split():
-        if word not in non_spoiler_words:
-            non_spoiler_words.append(word)
-        
-    answer = 0
-
-    for word in spoiler_words:
-        if word not in non_spoiler_words:
-            answer += 1
-
-    return answer
+        # 다음 단어
+        ws = we + 1
+            
+    return len(spoiler - normal)
